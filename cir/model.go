1abb047450740020390739f1d4d8b939943dfc23
elangovis <elango.v6@gmail.com>
2017-03-20 10:00:23 +0530

    struct simplify

--- cir/model.go
+++ cir/model.go
@@ -3,19 +3,10 @@ package cir
 type CIRDetails struct {
 
 	CaseNumber string `json:"caseNumber"`
-	CustomerName string `json:"customerName"`
+
 	Subject string `json:"subject"`
 	Description string `json:"description"`
-	IssueDate string `json:"issueDate"`
-	SiteName string `json:"siteName"`
-	UpdateDate string `json:"updatedate"`
-	ProductCompany string `json:"productCompany"`
-	CustomerWantDate string `json:"customerWantDate"`
-	EscalationLevel string `json:"escalationLevel"`
-	TypeOfIssue string `json:"typeOfIssue"`
-	Status string `json:"status"`
-	Aging string `json:"aging"`
-	ResolutionCommittedDate string `json:"resolutionCommittedDate"`
+
 
 }
 
